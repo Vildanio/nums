048df8e4735c4e25f60d43aa225773b365e615d3
kusamatovv <VildanKusamatov@yandex.ru>
2026-07-15 20:13:46 +0300

    Simplify Ratio

--- src/ratio.rs
+++ src/ratio.rs
@@ -8,51 +8,39 @@ impl Ratio {
     pub const HALF: Ratio = Self { value: 0.5 };
     pub const ONE: Ratio = Self { value: 1.0 };
 
-    pub const fn from_f64(value: f64) -> Option<Self> {
-        if value.is_normal() {
-            Some(Self { value })
-        } else {
-            None
-        }
-    }
-
-    pub const fn from_f64_unchecked(value: f64) -> Self {
-        if value.is_normal() {
-            Self { value }
-        } else {
-            panic!("Tried to create invalid ratio")
-        }
+    pub const fn from_f64(value: f64) -> Self {
+        Self { value }
     }
 
     pub const fn into_f64(self) -> f64 {
         self.value
     }
 
-    pub const fn negate(self) -> Option<Self> {
+    pub const fn negate(self) -> Self {
         Self::from_f64(-self.value)
     }
 
-    pub const fn add(self, rhs: Self) -> Option<Self> {
+    pub const fn add(self, rhs: Self) -> Self {
         Self::from_f64(self.value + rhs.value)
     }
 
-    pub const fn sub(self, rhs: Self) -> Option<Self> {
+    pub const fn sub(self, rhs: Self) -> Self {
         Self::from_f64(self.value - rhs.value)
     }
 
-    pub const fn mul(self, rhs: Self) -> Option<Self> {
+    pub const fn mul(self, rhs: Self) -> Self {
         Self::from_f64(self.value * rhs.value)
     }
 
-    pub const fn div(self, rhs: Self) -> Option<Self> {
+    pub const fn div(self, rhs: Self) -> Self {
         Self::from_f64(self.value / rhs.value)
     }
 
-    pub const fn mul_f64(self, rhs: f64) -> Option<Self> {
+    pub const fn mul_f64(self, rhs: f64) -> Self {
         Self::from_f64(self.value * rhs)
     }
 
-    pub const fn div_f64(self, rhs: f64) -> Option<Self> {
+    pub const fn div_f64(self, rhs: f64) -> Self {
         Self::from_f64(self.value / rhs)
     }
 }
